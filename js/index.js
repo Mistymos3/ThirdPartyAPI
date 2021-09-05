0c34170c0ca6cd01124e6cd1e005f9a3330caf02
Iva <84078385+Mistymos3@users.noreply.github.com>
2021-09-04 17:21:35 -0700

    added js code to change color blocks...

--- js/index.js
+++ js/index.js
@@ -1,5 +1,36 @@
-//set current day moment.js
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+// displays date
+var today = moment().format('dddd, MMM Do YYYY, h:mm a');
+$("#currentDay").html(today);
+
+
+// changes CSS styling depending on time
+    //check time, if in the past, present or future, remove or add class
+    function currentTime() {
+      var rightNow = moment().hour();
+console.log(rightNow)
+      $(".time-block").each(function(index) {
+          console.log(index);
+
+          var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+          console.log(blockTime)
+
+          // To check the time and add the classes to change the block colour 
+            if (blockTime < rightNow) {
+                $(this).addClass("past");
+            }
+            else if (blockTime === rightNow) {
+              $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+              $(this).removeClass("past");
+              $(this).removeClass("present");
+              $(this).removeClass("future");
+            }
+        })
+      }
+      currentTime();
+
 
 //save inputed data to local storage
 $(".saveBtn").on("click", function(){
@@ -25,31 +56,23 @@ $("#hour17 .description").val(localStorage.getItem("hour17"));
 
 // //set time blocks to past present or future class
 // function checkTime() {
-//     var currentTime = moment().hour();
+//   var rightNow = moment().hour();
+// // loop through time blocks
+//   $(".description .hour").each(function () {
+//       var block = parseInt($(this).attr("id").split("hour")[12]);
+// console.log
 
-// // var checkTime = function () {
-// //     //get current hour
-// //     var currentHour = moment().hour();
 
-// //     $("#currentDay").text();
-// // console.log(currentHour);
-
-// // loop over time blocks
-//     $(".time-block").each(function () {
-//         var block = parseInt($(this).attr("id").split("hour")[1]);
-
-//         //check if current time is past present or future to blockHour
-//         if (block < currentTime) {
-//             $(this).addClass("past").removeClass("present future");
-//         }
-//         else if (block === currentTime) {
-//             $(this).addClass("present").removeClass("past future");
-            
-//         }
-//         else {
-//             $(this).addClass("future").removeClass("past present");
-            
-//         }
-//     })
-//     checkTime();
+//       //check if current time is past present or future to blockHour
+//       if (block < rightNow) {
+//           $(this).addClass("past").removeClass("present future");
+//       }
+//       else if (block === rightNow) {
+//           $(this).addClass("present").removeClass("past future");
+//       }
+//       else {
+//           $(this).addClass("future").removeClass("past present");
+//       }
+//   })
 // }
+// checkTime();
